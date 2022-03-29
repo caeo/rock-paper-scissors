270d34c9f65d498a829c08355a5a6b99b324f484
Caio Silva <caeowul@gmail.com>
2022-03-29 18:36:40 -0300

    HTML

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -25,35 +25,80 @@ function playerChoice(choice){
     return pChoice
 }
 
+
+
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id)
+
+        const results = document.querySelector('.results')
+
+        let rodada = round(playerChoice(button.id), computerPlay())
+
+        const resultText = document.createTextNode(rodada)
+        results.append(resultText)
+
+        
+    })
+
+    
+})
+
+let scorePlayer = 0
+let scoreComputer = 0
+
+
+const updateScore = () =>{
+    const playerScore = document.querySelector('player-score p')
+    const computerScore = document.querySelector('computer-score p')
+    playerScore.textContent = scorePlayer;
+    computerScore.textContent = scoreComputer;
+}
+
 function round(playerChoice, computerPlay) {
     console.log(`Player choice: ${playerChoice} \n VS \n Computer Choice: ${computerPlay} \n`)
     let result = ''
     if (playerChoice == 'rock' && computerPlay == 'rock'){
         result = 'draw'
+        return result
     } else if (playerChoice == 'rock' && computerPlay == 'paper'){
         result = 'you lost'
+        scoreComputer += scoreComputer
+        updateScore()
+        return result
     } else if (playerChoice == 'rock' && computerPlay == 'scissors'){
         result = 'you won'
+        scorePlayer += scorePlayer
+        updateScore()
+        return result
     } else if (playerChoice == 'paper' && computerPlay == 'paper'){
         result = 'draw'
+        return result
     } else if (playerChoice == 'paper' && computerPlay == 'rock'){
         result = 'you won'
+        scorePlayer += scorePlayer
+        updateScore()
+        return result
     } else if (playerChoice == 'paper' && computerPlay == 'scissors'){
         result = 'you lost'
+        scoreComputer += scoreComputer
+        updateScore()
+        return result
     } else if (playerChoice == 'scissors' && computerPlay == 'scissors'){
         result = 'draw'
+        return result
     } else if (playerChoice == 'scissors' && computerPlay == 'rock'){
         result = 'you lost'
+        scoreComputer += scoreComputer
+        updateScore()
+        return result
     } else if (playerChoice == 'scissors' && computerPlay == 'paper'){
         result = 'you won'
-    } else{
-        result = 'what'
+        scorePlayer += scorePlayer
+        updateScore()
+        return result
     }
-    return result
+    return
 }
-
-let option = prompt('Choose your thing: \n 1 - rock \n 2 - paper \n 3 - scissors \n')
-
-let playerInstance = playerChoice(option)
-let computerInstance = computerPlay()
-console.log(round(playerInstance, computerInstance))
